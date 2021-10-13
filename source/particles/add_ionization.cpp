64678201676f4de2e003291790a2b73bf8a69bac
vladislav <crimsonschist@gmail.com>
2021-10-14 04:17:33 +0700

    cleared set_time_distribution

--- source/particles/add_ionization.cpp
+++ source/particles/add_ionization.cpp
@@ -30,11 +30,10 @@ std::vector<int> set_time_distribution(int t_inj, size_t total_number_of_particl
 	std::minstd_rand gen(42);
 	std::uniform_int_distribution<int> distribution(0, (t_inj-1));
 
-	// for (size_t n = 0; n < total_number_of_particles; n++) {
-		// array_of_particles_to_load[ distribution(gen) ] += 1;
-	// }
+	for (size_t n = 0; n < total_number_of_particles; n++) {
+		array_of_particles_to_load[ distribution(gen) ] += 1;
+	}
 
-	array_of_particles_to_load[ 3 ] = 50;
 	return array_of_particles_to_load;
 }
 
