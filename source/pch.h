2ee6d6f98b18e224f5da96fa34c6a1c8d1ca8605
vladislav <crimsonschist@gmail.com>
2022-08-13 08:52:11 +0700

    clean up on makefile, pch.h added

---
+++ source/pch.h
@@ -0,0 +1,18 @@
+#ifndef SRC_PCH_HPP
+#define SRC_PCH_HPP
+
+#include <omp.h>
+
+#include <cmath>
+
+#include <map>
+#include <list>
+#include <memory>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <functional>
+
+#include "constants.h"
+
+#endif  // SRC_PCH_HPP
