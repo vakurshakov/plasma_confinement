d04fba8d43460116fa785c85a55647511fdf2229
vladislav <crimsonschist@gmail.com>
2022-09-23 15:43:10 +0700

    small changes: commands cleaned up

--- src/command/set_particles.cpp
+++ src/command/set_particles.cpp
@@ -1,38 +1,34 @@
 #include "set_particles.hpp"
 
-#include "../particles/particle/particle.hpp"
+#include "src/particles/particle/point.hpp"
 
- 
-void Set_particles::execute(int _) const
-{
-    const double mass = particles->get_parameters().m();
-    const double Tx   = particles->get_parameters().Tx();
-    const double Ty   = particles->get_parameters().Ty();
-    const double Tz   = particles->get_parameters().Tz();
-    const double p0   = particles->get_parameters().p0();
-	const int    Np   = particles->get_parameters().Np();
+void Set_particles::execute(int /* timestep */) const {
+  PROFILE_FUNCTION();
 
-	particles->particles_.reserve( number_of_particles_to_load );
+  const double mass = particles->get_parameters().m();
+  const double Tx   = particles->get_parameters().Tx();
+  const double Ty   = particles->get_parameters().Ty();
+  const double Tz   = particles->get_parameters().Tz();
+  const double p0   = particles->get_parameters().p0();
+  const int    Np   = particles->get_parameters().Np();
 
-	for (int nx = 0; nx < SIZE_X; ++nx) {
-	for (int ny = 0; ny < SIZE_Y; ++ny) {
-		
-		if ( this_is_config_cell(nx, ny) )
-        {
-			for (int i = 0; i < Np; ++i)
-            {
-				double x, y;
-				fill_the_cell(i, Np, nx, ny, &x, &y);
+  particles->particles_.reserve(number_of_particles_to_load);
 
-				double px, py, pz;
-				do
-                {
-                    load_impulse(x, y, mass, Tx, Ty, Tz, p0, &px, &py, &pz);
-                }
-                while (std::isinf(px) || std::isinf(py) || std::isinf(pz));
+  for (int nx = 0; nx < SIZE_X; ++nx) {
+  for (int ny = 0; ny < SIZE_Y; ++ny) {
+    if (!this_is_config_cell(nx, ny)) continue;
 
-				particles->add_particle(Point({x, y}, {px, py, pz}));
-			}
-		}
-	}}
+    for (int i = 0; i < Np; ++i) {
+      double x, y;
+      fill_the_cell(i, Np, nx, ny, &x, &y);
+
+      double px, py, pz;
+      do {
+        load_impulse(x, y, mass, Tx, Ty, Tz, p0, &px, &py, &pz);
+      }
+      while (std::isinf(px) || std::isinf(py) || std::isinf(pz));
+
+      particles->add_particle({{x, y}, {px, py, pz}});
+    }
+  }}
 }
--- src/command/set_particles.hpp
+++ src/command/set_particles.hpp
@@ -1,73 +1,67 @@
-#ifndef COMMAND_SET_PARTICLES_HPP
-#define COMMAND_SET_PARTICLES_HPP
-
-//#######################################################################################
+#ifndef SRC_COMMAND_SET_PARTICLES_HPP
+#define SRC_COMMAND_SET_PARTICLES_HPP
 
 #include "command.hpp"
 
 #include "src/pch.h"
-#include "../particles/particles.hpp"
+#include "src/particles/particles.hpp"
 
 /**
  * @brief Command that sets particles before the main calculation cycle.
  */
 class Set_particles : public Command {
-public:
-    /**
-     * @brief Constructor of the command.
-     * 
-     * @param particles Pointer on particles to be set. 
-     * @param number_of_particles_to_load Precaclulated shape of a
-     *      resulted vector. 
-     * @param this_is_config_cell Function that indicates a cell
-     *      is in configuration when the cells pass through each one in domain.
-     * @param load_impulse Impulse distribution function.
-     * @param fill_the_cell Way of filling particles in the cell.
-     * 
-     * @warning Total number of particles may be not equal to 
-     *      number_of_particles_to_load, each cell that is in configuration
-     *      fills with Np particles.
-     */
-	Set_particles(
-		Particles* const particles,
-        std::size_t number_of_particles_to_load,
-        std::function<bool(int cell_number_nx, int cell_number_ny)> this_is_config_cell,
-        std::function<void(double x, double y,
-		  double mass, double Tx, double Ty, double Tz,
-		  double p0, double* px, double* py, double* pz)> load_impulse,
-        std::function<void(int sequential_number, int Np,
-		  int cell_number_nx, int cell_number_ny,
-		  double* x, double* y)> fill_the_cell )
-		    :	particles(particles),
-                number_of_particles_to_load(number_of_particles_to_load),
-                this_is_config_cell(this_is_config_cell),
-                load_impulse(load_impulse),
-                fill_the_cell(fill_the_cell) {};
-
-    /**
-     * @brief Passes through each cell in the domain and if the
-     *      cell is in config, it puts there Np particles with
-     *      chosen impulse distribution and way of filling.
-     * @param _ Placeholder for the time, pass 0.
-     */
-	void execute(int _) const override;
-
-private:
-	Particles* const particles;
-    
-    std::size_t number_of_particles_to_load;
-    
-    std::function<bool(int cell_number_nx, int cell_number_ny)> this_is_config_cell;
-
+ public:
+  /**
+   * @brief Constructor of the command.
+   *
+   * @param particles Pointer on particles to be set.
+   * @param number_of_particles_to_load Precalculated shape of a resulted vector.
+   * @param this_is_config_cell Function that indicates a cell
+   * is in configuration when the cells pass through each one in domain.
+   * @param load_impulse Impulse distribution function.
+   * @param fill_the_cell Way of filling particles in the cell.
+   *
+   * @warning Total number of particles may be not equal to
+   * @a number_of_particles_to_load, each cell that is in configuration
+   * fills with Np particles.
+   */
+  Set_particles(
+    Particles* const particles,
+    std::size_t number_of_particles_to_load,
+    std::function<bool(int cell_number_nx, int cell_number_ny)> this_is_config_cell,
     std::function<void(double x, double y,
-	  double mass, double Tx, double Ty, double Tz,
-	  double p0, double* px, double* py, double* pz)> load_impulse;
-    
+      double mass, double Tx, double Ty, double Tz,
+      double p0, double* px, double* py, double* pz)> load_impulse,
     std::function<void(int sequential_number, int Np,
-	  int cell_number_nx, int cell_number_ny,
-	  double* x, double* y)> fill_the_cell;    
+      int cell_number_nx, int cell_number_ny,
+      double* x, double* y)> fill_the_cell )
+    :	particles(particles),
+      number_of_particles_to_load(number_of_particles_to_load),
+      this_is_config_cell(this_is_config_cell),
+      load_impulse(load_impulse),
+      fill_the_cell(fill_the_cell) {};
+
+  /**
+   * @brief Passes through each cell in the domain and if the
+   * cell is in config, it puts there Np particles with
+   * chosen impulse distribution and way of filling.
+   */
+  void execute(int /* timestep */) const override;
+
+ private:
+  Particles* const particles;
+
+  std::size_t number_of_particles_to_load;
+
+  std::function<bool(int cell_number_nx, int cell_number_ny)> this_is_config_cell;
+
+  std::function<void(double x, double y,
+    double mass, double Tx, double Ty, double Tz,
+    double p0, double* px, double* py, double* pz)> load_impulse;
+
+  std::function<void(int sequential_number, int Np,
+    int cell_number_nx, int cell_number_ny,
+    double* x, double* y)> fill_the_cell;
 };
 
-//#######################################################################################
-
-#endif // COMMAND_SET_PARTICLES_HPP
+#endif  // SRC_COMMAND_SET_PARTICLES_HPP
