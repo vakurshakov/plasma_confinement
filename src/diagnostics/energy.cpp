8c8e3f1046c8f58979107468cc2e5a74b5ffad4f
vladislav <crimsonschist@gmail.com>
2023-03-11 09:15:36 +0700

    collecting also sum of (B(x, y) - B_0)^2

--- src/diagnostics/energy.cpp
+++ src/diagnostics/energy.cpp
@@ -1,6 +1,13 @@
 #include "energy.hpp"
 #include "src/file_writers/bin_file.hpp"
 
+#if !BEAM_INJECTION_SETUP
+
+#include "src/utils/transition_layer/table_function.hpp"
+static auto __Bz = Table_function("src/utils/transition_layer/Bz_" + config::postfix);
+
+#endif
+
 namespace fs = std::filesystem;
 
 energy_parameters_saver::energy_parameters_saver(
@@ -26,33 +33,52 @@ fields_energy::fields_energy(
 void fields_energy::diagnose(int t) {
   PROFILE_FUNCTION();
 
-  double Wex = 0, Wey = 0, Wez = 0;
-  double Wbx = 0, Wby = 0, Wbz = 0;
+  double WEx = 0, WEy = 0, WEz = 0;
+  double WBx = 0, WBy = 0, WBz = 0, WdBz = 0;
 
-  #pragma omp parallel for reduction(+: Wex, Wey, Wez, Wbx, Wby, Wbz)
+  #pragma omp parallel for reduction(+: WEx, WEy, WEz, WBx, WBy, WBz, WdBz)
   for (int y = 0; y < SIZE_Y; ++y) {
   for (int x = 0; x < SIZE_X; ++x) {
-    Wex += 0.5 * electric_.x(y, x) * electric_.x(y, x) * dx * dy;
-    Wey += 0.5 * electric_.y(y, x) * electric_.y(y, x) * dx * dy;
-    Wez += 0.5 * electric_.z(y, x) * electric_.z(y, x) * dx * dy;
+    WEx += 0.5 * electric_.x(y, x) * electric_.x(y, x) * dx * dy;
+    WEy += 0.5 * electric_.y(y, x) * electric_.y(y, x) * dx * dy;
+    WEz += 0.5 * electric_.z(y, x) * electric_.z(y, x) * dx * dy;
 
-    Wbx += 0.5 * magnetic_.x(y, x) * magnetic_.x(y, x) * dx * dy;
-    Wby += 0.5 * magnetic_.y(y, x) * magnetic_.y(y, x) * dx * dy;
-    Wbz += 0.5 * magnetic_.z(y, x) * magnetic_.z(y, x) * dx * dy;
+    WBx += 0.5 * magnetic_.x(y, x) * magnetic_.x(y, x) * dx * dy;
+    WBy += 0.5 * magnetic_.y(y, x) * magnetic_.y(y, x) * dx * dy;
+    WBz += 0.5 * magnetic_.z(y, x) * magnetic_.z(y, x) * dx * dy;
+
+#if BEAM_INJECTION_SETUP
+    WdBz += 0.5 * (magnetic_.z(y, x) - config::Omega_max) * (magnetic_.z(y, x) - config::Omega_max) * dx * dy;
+#else
+    double B0 = 0.0;
+
+    if (x * dx < __Bz.get_x0()) {
+      B0 = 0.0;
+    }
+    else if (x * dx < __Bz.get_xmax()) {
+      B0 = __Bz(x * dx);
+    }
+    else {
+      B0 = __Bz(__Bz.get_xmax());
+    }
+
+    WdBz += 0.5 * (magnetic_.z(y, x) - B0) * (magnetic_.z(y, x) - B0) * dx * dy;
+#endif
   }}
 
-  file_for_results_->write(Wex);
-  file_for_results_->write(Wey);
-  file_for_results_->write(Wez);
-  file_for_results_->write(Wbx);
-  file_for_results_->write(Wby);
-  file_for_results_->write(Wbz);
+  file_for_results_->write(WEx);
+  file_for_results_->write(WEy);
+  file_for_results_->write(WEz);
+  file_for_results_->write(WBx);
+  file_for_results_->write(WBy);
+  file_for_results_->write(WBz);
+  file_for_results_->write(WdBz);
 
-  file_for_results_->write(Wex + Wey + Wez + Wbx + Wby + Wbz);
+  file_for_results_->write(WEx + WEy + WEz + WBx + WBy + WBz);
 
-  LOG_INFO("Fields energy: Ex = {:.5e}, Ey = {:.5e}, Ez = {:.5e}", Wex, Wey, Wez);
-  LOG_INFO("               Bx = {:.5e}, By = {:.5e}, Bz = {:.5e}", Wbx, Wby, Wbz);
-  LOG_INFO("            Total = {}", Wex + Wey + Wez + Wbx + Wby + Wbz);
+  LOG_INFO("Fields energy: Ex = {:.5e}, Ey = {:.5e}, Ez = {:.5e}", WEx, WEy, WEz);
+  LOG_INFO("               Bx = {:.5e}, By = {:.5e}, Bz = {:.5e}", WBx, WBy, WBz);
+  LOG_INFO("            Total = {}", WEx + WEy + WEz + WBx + WBy + WBz);
 
   if (t % diagnose_time_step == 0)
     file_for_results_->flush();
