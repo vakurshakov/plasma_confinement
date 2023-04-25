6dbbe11548024f7f60a6ba08e2dfe7707a4e7aca
Vladislav Kurshakov <kurshakov@annenkov-lab10.BINP>
2023-04-25 18:42:14 +0700

    precursive (not compiled) changes in initialization method

--- src/managers/manager.cpp
+++ src/managers/manager.cpp
@@ -30,31 +30,8 @@ void Manager::log_information() const {
   LOG_INFO("  length along x axis,  Lx = {:.2e} c/w_pe ({} cells)", SIZE_X * dx, SIZE_X);
   LOG_INFO("  length along y axis,  Ly = {:.2e} c/w_pe ({} cells)", SIZE_Y * dy, SIZE_Y);
   LOG_INFO("  total simulation time, T = {:.2e} 1/w_pe ({} timesteps)", TIME * dt, TIME);
-  LOG_INFO("  magnetic field,    B_max = {:.2e} m_e*c*w_pe/e", config::Omega_max);
+  // LOG_INFO("  magnetic field,    B_max = {:.2e} m_e*c*w_pe/e", config::Omega_max);
 
-  LOG_INFO("Plasma ions:");
-  LOG_INFO("  temperature,              T_i = {:.2e} KeV", config::T_ions);
-  LOG_INFO("  thermal velocity,        v_Ti = {:.2e} c", config::V_ions);
-  LOG_INFO("  Debye length,            L_di = {:.2e} c/w_pe", config::V_ions * sqrt(config::mi_me));
-  LOG_INFO("  cyclotron frequency,     Om_i = {:.2e} w_pe", config::Omega_max / config::mi_me);
-  LOG_INFO("  cyclotron period, 2 pi / Om_i = {:.2e} 1/w_pe ({} timesteps)",
-    2 * M_PI / (config::Omega_max / config::mi_me),
-    int(2 * M_PI / (config::Omega_max / config::mi_me) / dt));
-  LOG_INFO("  cyclotron radius,       rho_i = {:.2e} c/w_pe ({} cells)",
-    config::V_ions * config::mi_me / config::Omega_max,
-    int(config::V_ions * config::mi_me / config::Omega_max / dx));
-
-  LOG_INFO("Plasma electrons:");
-  LOG_INFO("  temperature,              T_e = {:.2e} KeV", config::T_electrons);
-  LOG_INFO("  thermal velocity,        v_Te = {:.2e} c", config::V_electrons);
-  LOG_INFO("  Debye length,            L_de = {:.2e} c/w_pe", config::V_electrons);
-  LOG_INFO("  cyclotron frequency,     Om_e = {:.2e} w_pe", config::Omega_max);
-  LOG_INFO("  cyclotron period, 2 pi / Om_e = {:.2e} 1/w_pe ({} timesteps)",
-    2 * M_PI / (config::Omega_max),
-    int(2 * M_PI / (config::Omega_max) / dt));
-  LOG_INFO("  cyclotron radius,       rho_e = {:.2e} c/w_pe ({} cells)",
-    config::V_electrons / config::Omega_max,
-    int(config::V_electrons / config::Omega_max / dx));
   LOG_FLUSH();
 }
 
@@ -70,19 +47,11 @@ void Manager::initializes() {
   Fields_builder fields_builder;
   fields_ = Fields(fields_builder);
 
-#if there_are_fields
   // We use this to push the magnetic field to
   // the same half-timestep as electric field
   step_presets_.push_front(std::make_unique<Magnetic_field_half_step>(&fields_));
 
-#if there_are_Bz0 && !START_FROM_BACKUP
   presets.push_back(std::make_unique<Set_Bz_distribution>(&fields_));
-#endif
-#endif
-
-#if START_FROM_BACKUP
-  Simulation_backup::restore_time_diagnostics();
-#endif
 
   Particles_builder particles_builder(fields_);
 
@@ -93,20 +62,9 @@ void Manager::initializes() {
     config::domain_phi_max
   };
 
-#if !there_are_target_plasma
-  particles_species_.reserve(2);
-#else
-  particles_species_.reserve(4);
-#endif
-
-  particles_builder.set_sort("plasma_ions");
-  Particles& plasma_ions = particles_species_.emplace_back(particles_builder);
-
-  plasma_ions.boundaries_processor_ = std::make_unique<POL_Beam_boundary>(
-    plasma_ions.particles_, plasma_ions.parameters_, domain);
-
-  plasma_ions.particles_.reserve(config::PER_STEP_PARTICLES * TIME + 10'000);
-
+  std::unordered_map<std::string, Particles&>{
+    // ???
+  },
 
   particles_builder.set_sort("plasma_electrons");
   Particles& plasma_electrons = particles_species_.emplace_back(particles_builder);
@@ -116,79 +74,13 @@ void Manager::initializes() {
 
   plasma_electrons.particles_.reserve(config::PER_STEP_PARTICLES * TIME + 10'000);
 
-  auto generator_of_circle_setter = [] (double R_max) {
-    return [=] (double *x, double *y) {
-      static const double center_x = 0.5 * SIZE_X * dx;
-      static const double center_y = 0.5 * SIZE_Y * dy;
-
-      double r = R_max * sqrt(random_01());
-      double phi = 2.0 * M_PI * random_01();
-
-      *x = center_x + r * cos(phi);
-      *y = center_y + r * sin(phi);
-    };
-  };
-
-  step_presets_.emplace_back(std::make_unique<Ionize_particles>(
-    &plasma_ions, &plasma_electrons,
-    set_time_distribution(TIME, config::PER_STEP_PARTICLES * TIME),
-    generator_of_circle_setter(config::RADIUS_OF_INJECTION_AREA),
-    uniform_probability,
-    load_maxwellian_impulse
-  ));
-
-
-#if there_are_target_plasma
-  particles_builder.set_sort("target_ions");
-  Particles& target_ions = particles_species_.emplace_back(particles_builder);
-
-  target_ions.boundaries_processor_ = std::make_unique<POL_Beam_boundary>(
-    target_ions.particles_, target_ions.parameters_, domain);
-
-  const int total_number_of_ions =
-    M_PI * config::RADIUS_OF_TARGET_PLASMA * config::RADIUS_OF_TARGET_PLASMA * config::Npi / (dx * dy);
-
-  struct Set_coordinate_on_circle : public Coordinate_generator {
-    using coordinate_loader = std::function<void(double*, double*)>;
-    coordinate_loader implementation_;
-
-    Set_coordinate_on_circle(const coordinate_loader& implementation)
-      : implementation_(implementation) {}
-
-    void load(double* x, double* y) override {
-      implementation_(x, y);
-    }
-  };
-
-  presets.emplace_back(std::make_unique<Set_particles>(
-    &target_ions, total_number_of_ions,
-    std::make_unique<Set_coordinate_on_circle>(
-      generator_of_circle_setter(config::RADIUS_OF_TARGET_PLASMA)),
-    load_maxwellian_impulse));
-
-
-  particles_builder.set_sort("target_electrons");
-  Particles& target_electrons = particles_species_.emplace_back(particles_builder);
-
-  target_electrons.boundaries_processor_ = std::make_unique<POL_Beam_boundary>(
-    target_electrons.particles_, target_electrons.parameters_, domain);
-
-  presets.emplace_back(std::make_unique<Copy_coordinates>(
-    &target_electrons, &target_ions, load_maxwellian_impulse));
-
-#endif
 
 #if MAKE_BACKUPS || START_FROM_BACKUP
   auto backup =  std::make_unique<Simulation_backup>(
     /* backup timestep = */ 100 * diagnose_time_step,
     // named particle species to backup:
     std::unordered_map<std::string, Particles&>{
-      { plasma_ions.sort_name_, plasma_ions },
-      { plasma_electrons.sort_name_, plasma_electrons },
-#if there_are_target_plasma
-      { target_ions.sort_name_, target_ions },
-      { target_electrons.sort_name_, target_electrons },
-#endif
+      // ???
     },
     // named fields to backup:
     std::unordered_map<std::string, vector3_field&>{
