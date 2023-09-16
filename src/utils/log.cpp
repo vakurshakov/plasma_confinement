718daf6065e8284a202b7ba0c8016b3d9fb2bf3c
vladislav <crimsonschist@gmail.com>
2023-09-16 17:08:13 +0700

    logging pattern changed

--- src/utils/log.cpp
+++ src/utils/log.cpp
@@ -8,7 +8,7 @@ void Log::Init(const std::string& filename) {
   // basic logger that will write everything to %filename%
   logger_ = spdlog::basic_logger_mt("Plasma_simulation", filename, /* truncate = */ true);
 
-  logger_->set_pattern("%^[%D %T]: %v %$");
+  logger_->set_pattern("%^[%m/%d, %R, %L]%$ %v");
 
   logger_->set_level(spdlog::level::trace);
   logger_->flush_on(spdlog::level::warn);
