d81454f9542541a38655f27dcb2a25b3e5d74686
vladislav <crimsonschist@gmail.com>
2023-09-23 14:07:30 +0700

    v3f[width, width] replaced with smaller double[width] to avoid indirect calls cost

--- src/solvers/Esirkepov_density_decomposition.cpp
+++ src/solvers/Esirkepov_density_decomposition.cpp
@@ -4,7 +4,7 @@ Esirkepov_density_decomposition::Esirkepov_density_decomposition(
   const Parameters& parameters, vector3_field& J)
   : Np_(parameters.Np()), J_(J) {}
 
-static const int shape_width = 2 * shape_radius + 1;
+static constexpr int shape_width = 2 * shape_radius + 1;
 
 void Esirkepov_density_decomposition::process(const Particle& particle, const vector2& r0) {
   double n = particle.n();
@@ -13,34 +13,34 @@ void Esirkepov_density_decomposition::process(const Particle& particle, const ve
   const int node_px = int(round(particle.point.x() / dx));
   const int node_py = int(round(particle.point.y() / dy));
 
-  v3f temp_J(shape_width, shape_width);
-
-  decompose_x(particle, r0, n, q, node_px, node_py, temp_J);
-  decompose_y(particle, r0, n, q, node_px, node_py, temp_J);
+  decompose_x(particle, r0, n, q, node_px, node_py);
+  decompose_y(particle, r0, n, q, node_px, node_py);
 
 #if _2D3V
-  decompose_z(particle, r0, n, q, node_px, node_py, temp_J);
+  decompose_z(particle, r0, n, q, node_px, node_py);
 #endif
 }
 
 void Esirkepov_density_decomposition::decompose_x(
     const Particle& particle, const vector2& r0,
-    double n, double q, int node_px, int node_py, v3f& temp_J) {
+    double n, double q, int node_px, int node_py) {
+  double temp_Jx[shape_width];
+
   const vector2& r = particle.point.r;
 
   int node_gx, node_gy;
   node_gx = node_px - shape_radius;
 
-  for (int x = 0, y = 0; y < shape_width; ++y) {
+  for (int y = 0; y < shape_width; ++y) {
     node_gy = node_py + (y - shape_radius);
 
     // Give up trying to put (node_gx + 0.5) here
-    temp_J.x(y,x) = - q * n/Np_ * 0.5 * dx / dt *
+    temp_Jx[y] = - q * n/Np_ * 0.5 * dx / dt *
       (shape_function(r.x() - node_gx * dx, dx) - shape_function(r0.x() - node_gx * dx, dx)) *
       (shape_function(r.y() - node_gy * dy, dy) + shape_function(r0.y() - node_gy * dy, dy));
 
     #pragma omp atomic
-    J_.x(node_gy,node_gx) += temp_J.x(y,x);
+    J_.x(node_gy, node_gx) += temp_Jx[y];
   }
 
   for (int y = 0; y < shape_width; ++y) {
@@ -49,36 +49,37 @@ void Esirkepov_density_decomposition::decompose_x(
     for (int x = 1; x < shape_width; ++x) {
       node_gx = node_px + (x - shape_radius);
 
-      // Here is only "=" sign (not "+=")
       // Give up trying to put (node_gx + 0.5) here
-      temp_J.x(y,x) = temp_J.x(y,x-1) - q * n/Np_ * 0.5 * dx / dt *
+      temp_Jx[y] += - q * n/Np_ * 0.5 * dx / dt *
         (shape_function(r.x() - node_gx * dx, dx) - shape_function(r0.x() - node_gx * dx, dx)) *
         (shape_function(r.y() - node_gy * dy, dy) + shape_function(r0.y() - node_gy * dy, dy));
 
       #pragma omp atomic
-      J_.x(node_gy, node_gx) += temp_J.x(y,x);
+      J_.x(node_gy, node_gx) += temp_Jx[y];
     }
   }
 }
 
 void Esirkepov_density_decomposition::decompose_y(
     const Particle& particle, const vector2& r0,
-    double n, double q, int node_px, int node_py, v3f& temp_J) {
+    double n, double q, int node_px, int node_py) {
+  double temp_Jy[shape_width];
+
   const vector2& r = particle.point.r;
 
   int node_gx, node_gy;
   node_gy = node_py - shape_radius;
 
-  for (int y = 0, x = 0; x < shape_width; ++x) {
+  for (int x = 0; x < shape_width; ++x) {
     node_gx = node_px + (x - shape_radius);
 
     // Give up trying to put (node_gy + 0.5) here
-    temp_J.y(y,x) = - q * n/Np_ * 0.5 * dy / dt *
+    temp_Jy[x] = - q * n/Np_ * 0.5 * dy / dt *
       (shape_function(r.x() - node_gx * dx, dx) + shape_function(r0.x() - node_gx * dx, dx)) *
       (shape_function(r.y() - node_gy * dy, dy) - shape_function(r0.y() - node_gy * dy, dy));
 
     #pragma omp atomic
-    J_.y(node_gy, node_gx) += temp_J.y(y,x);
+    J_.y(node_gy, node_gx) += temp_Jy[x];
   }
 
   for (int y = 1; y < shape_width; ++y) {
@@ -87,14 +88,13 @@ void Esirkepov_density_decomposition::decompose_y(
     for (int x = 0; x < shape_width; ++x) {
       node_gx = node_px + (x - shape_radius);
 
-      // Here is only "=" sign (not "+=")
       // Give up trying to put (node_gy + 0.5) here
-      temp_J.y(y,x) = temp_J.y(y-1,x) - q * n/Np_ * 0.5 * dy / dt *
+      temp_Jy[x] += - q * n/Np_ * 0.5 * dy / dt *
         (shape_function(r.x() - node_gx * dx, dx) + shape_function(r0.x() - node_gx * dx, dx)) *
         (shape_function(r.y() - node_gy * dy, dy) - shape_function(r0.y() - node_gy * dy, dy));
 
       #pragma omp atomic
-      J_.y(node_gy, node_gx) += temp_J.y(y,x);
+      J_.y(node_gy, node_gx) += temp_Jy[x];
     }
   }
 }
@@ -102,14 +102,14 @@ void Esirkepov_density_decomposition::decompose_y(
 
 void Esirkepov_density_decomposition::decompose_z(
     const Particle& particle, const vector2& r0,
-    double n, double q, int node_px, int node_py, v3f& temp_J) {
-  double m = particle.m();
+    double n, double q, int node_px, int node_py) {
+  double temp_Jz;
 
   const vector2& r = particle.point.r;
   const vector3& p = particle.point.p;
 
-  double gamma_m = sqrt(m * m + p.dot(p));
-  double vz = p.z() / gamma_m;
+  double m = particle.m();
+  double vz = p.z() / sqrt(m * m + p.dot(p));
 
   int node_gx, node_gy;
 
@@ -119,14 +119,14 @@ void Esirkepov_density_decomposition::decompose_z(
     for (int x = 0; x < shape_width; ++x) {
       node_gx = node_px + (x - shape_radius);
 
-      temp_J.z(y,x) = q * n/Np_ * vz * (
+      temp_Jz = q * n/Np_ * vz * (
         shape_function( r.x() - (node_gx + 0.5) * dx, dx) * shape_function( r.y() - (node_gy + 0.5) * dy, dy) / 3. +
         shape_function(r0.x() - (node_gx + 0.5) * dx, dx) * shape_function( r.y() - (node_gy + 0.5) * dy, dy) / 6. +
         shape_function( r.x() - (node_gx + 0.5) * dx, dx) * shape_function(r0.y() - (node_gy + 0.5) * dy, dy) / 6. +
         shape_function(r0.x() - (node_gx + 0.5) * dx, dx) * shape_function(r0.y() - (node_gy + 0.5) * dy, dy) / 3.);
 
       #pragma omp atomic
-      J_.z(node_gy, node_gx) += temp_J.z(y,x);
+      J_.z(node_gy, node_gx) += temp_Jz;
     }
   }
 }
--- src/solvers/Esirkepov_density_decomposition.hpp
+++ src/solvers/Esirkepov_density_decomposition.hpp
@@ -13,10 +13,9 @@ class Esirkepov_density_decomposition : public Decomposition {
   void process(const Particle&, const vector2& r0) override;
 
  private:
-  using v3f = vector3_field;
-  void decompose_x(const Particle&, const vector2& r0, double n, double q, int nx, int ny, v3f& temp_J);
-  void decompose_y(const Particle&, const vector2& r0, double n, double q, int nx, int ny, v3f& temp_J);
-  void decompose_z(const Particle&, const vector2& r0, double n, double q, int nx, int ny, v3f& temp_J);
+  void decompose_x(const Particle&, const vector2& r0, double n, double q, int nx, int ny);
+  void decompose_y(const Particle&, const vector2& r0, double n, double q, int nx, int ny);
+  void decompose_z(const Particle&, const vector2& r0, double n, double q, int nx, int ny);
 
   const int Np_;
   vector3_field& J_;
