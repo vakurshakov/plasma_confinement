b0bea2283b00b679a395a15e2ebc26db5fd12ff6
vladislav <crimsonschist@gmail.com>
2023-09-26 13:58:29 +0700

    naming changed

--- src/solvers/simple_interpolation.cpp
+++ src/solvers/simple_interpolation.cpp
@@ -10,27 +10,27 @@ enum shift {
 };
 
 void Simple_interpolation::process(const vector2& r0, vector3& local_E, vector3& local_B) const {
-  const int nearest_x = int(round(r0.x()/dx));
-  const int nearest_y = int(round(r0.y()/dy));
+  const int n_px = int(round(r0.x() / dx));
+  const int n_py = int(round(r0.y() / dy));
 
   vector2 shape[2];
 
-  for(int ny = nearest_y - shape_radius; ny <= nearest_y + shape_radius; ++ny) {
-    shape[shift::no].y() = shape_function(ny * dy - r0.y(), dy);
-    shape[shift::sh].y() = shape_function((ny + 0.5) * dy - r0.y(), dy);
+  for(int y = n_py - shape_radius; y <= n_py + shape_radius; ++y) {
+    shape[shift::no].y() = shape_function(y * dy - r0.y(), dy);
+    shape[shift::sh].y() = shape_function((y + 0.5) * dy - r0.y(), dy);
 
-    for(int nx = nearest_x - shape_radius; nx <= nearest_x + shape_radius; ++nx) {
-      shape[shift::no].x() = shape_function(nx * dx - r0.x(), dx);
-      shape[shift::sh].x() = shape_function((nx + 0.5) * dx - r0.x(), dx);
+    for(int x = n_px - shape_radius; x <= n_px + shape_radius; ++x) {
+      shape[shift::no].x() = shape_function(x * dx - r0.x(), dx);
+      shape[shift::sh].x() = shape_function((x + 0.5) * dx - r0.x(), dx);
 
-      local_E.x() += E_.x(ny,nx) * shape[shift::no].y() * shape[shift::sh].x();
-      local_E.y() += E_.y(ny,nx) * shape[shift::sh].y() * shape[shift::no].x();
-      local_B.z() += B_.z(ny,nx) * shape[shift::sh].y() * shape[shift::sh].x();
+      local_E.x() += E_.x(y,x) * shape[shift::no].y() * shape[shift::sh].x();
+      local_E.y() += E_.y(y,x) * shape[shift::sh].y() * shape[shift::no].x();
+      local_B.z() += B_.z(y,x) * shape[shift::sh].y() * shape[shift::sh].x();
 
 #if _2D3V
-      local_E.z() += E_.z(ny,nx) * shape[shift::sh].y() * shape[shift::sh].x();
-      local_B.x() += B_.x(ny,nx) * shape[shift::no].y() * shape[shift::sh].x();
-      local_B.y() += B_.y(ny,nx) * shape[shift::sh].y() * shape[shift::no].x();
+      local_E.z() += E_.z(y,x) * shape[shift::sh].y() * shape[shift::sh].x();
+      local_B.x() += B_.x(y,x) * shape[shift::no].y() * shape[shift::sh].x();
+      local_B.y() += B_.y(y,x) * shape[shift::sh].y() * shape[shift::no].x();
 #endif
     }
   }
